199b5d19f6ebde338d6a0e885fbdc7426f41081b
Gaurav-Pawar25 <gauravpawar33915@gmail.com>
2024-08-08 22:47:10 +0530

    Done

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -1,5 +1,5 @@
-
 import java.util.List;
+import java.util.Collections;
 
 public class LargestSum {
     /**
@@ -9,7 +9,29 @@ public class LargestSum {
      * @param nums a list of ints.
      * @return the largest possible sum of separate numbers from nums.
      */
-    public int bigSum(List<Integer> nums){
-        return 0;
+    public int bigSum(List<Integer> nums) {
+
+
+        // Sort the list in descending order
+        Collections.sort(nums, Collections.reverseOrder());
+
+        // Take the first element as the largest
+        int first = nums.get(0);
+        int second = Integer.MIN_VALUE;
+
+        // Find the second largest value, allowing for duplicates
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i) != first || (i > 0 && nums.get(i) == first)) {
+                second = nums.get(i);
+                break;
+            }
+        }
+
+        // If no distinct or valid second value is found, second will still be the first
+        if (second == Integer.MIN_VALUE) {
+            second = first;
+        }
+
+        return first + second;
     }
 }
